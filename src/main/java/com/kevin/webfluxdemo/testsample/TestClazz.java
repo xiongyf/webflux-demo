584e143f8cfcde3219fc83d44d0e877d992ee957
xiongyf <xiongyf1@gmail.com>
2019-07-23 00:00:24 +0800

    add extract list method

--- src/main/java/com/kevin/webfluxdemo/testsample/TestClazz.java
+++ src/main/java/com/kevin/webfluxdemo/testsample/TestClazz.java
@@ -4,6 +4,9 @@ package com.kevin.webfluxdemo.testsample;
 import com.kevin.webfluxdemo.sys.pojo.User;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestClazz {
 
     public void lambadaTest() {
@@ -23,7 +26,6 @@ public class TestClazz {
         lambadaInterface.hello("Victoria");
     }
 
-    @Test
     public void doubleColon() {
         IConvert<String, User> convert = this::say;
         User user = convert.convert("Tom");
@@ -31,10 +33,41 @@ public class TestClazz {
     }
 
     public User say(String name) {
-        User user=new User();
+        User user = new User();
         user.setName(name);
         return user;
     }
 
+
+    @Test
+    public void extract() {
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            strings.add(i + "");
+        }
+        System.out.println(extractList(strings, 11, 20));
+    }
+
+    public List extractList(List list, int start, int num) {
+        if (list == null || list.size() < 1) {
+            return null;
+        }
+        int size = list.size();
+        if (size < num) {
+            return list;
+        }
+        List subList = new ArrayList();
+        if (start + num < size) {
+            for (int i = start - 1; i < start + num - 1; i++) {
+                subList.add(list.get(i));
+            }
+        } else {
+            for (int i = start - 1; i < list.size(); i++) {
+                subList.add(list.get(i));
+            }
+        }
+
+        return subList;
+    }
 
 }
