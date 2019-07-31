cc81786b0846809cf09bee34494f449470a42117
xiongyf <xiongyf1@gmail.com>
2019-07-31 22:10:20 +0800

    fix: when start and num is 1 returns null

--- src/main/java/com/kevin/webfluxdemo/utils/ListUtils.java
+++ src/main/java/com/kevin/webfluxdemo/utils/ListUtils.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListUtils {
-    public static  <T> List pagingList(List<T> list, int start, int num) {
+    public static <T> List pagingList(List<T> list, int start, int num) {
         if (list == null || list.size() < 1) {
             return null;
         }
         int size = list.size();
-        if(start<1){
-            start=1;
+        if (start < 1) {
+            start = 1;
         }
         List<T> subList = new ArrayList<>();
         if (size / num == 0) {
-            if (start < size) {
-                subList = list.subList(start-1,size);
+            if (start <= size) {
+                subList = list.subList(start - 1, size);
             }
         } else if (size / num > 0) {
             if (size >= start + num) {
