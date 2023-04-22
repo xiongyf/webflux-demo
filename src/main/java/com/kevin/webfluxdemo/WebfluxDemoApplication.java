850a52abfb18ae4f54718369eba1e5f6d4291500
xiongyf <1228761610@qq.com>
2023-04-22 12:30:09 +0800

    upgrade and format

--- src/main/java/com/kevin/webfluxdemo/WebfluxDemoApplication.java
+++ src/main/java/com/kevin/webfluxdemo/WebfluxDemoApplication.java
@@ -2,10 +2,8 @@ package com.kevin.webfluxdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry
 public class WebfluxDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kevin/webfluxdemo/sys/controller/UserController.java
+++ src/main/java/com/kevin/webfluxdemo/sys/controller/UserController.java
@@ -15,33 +15,22 @@ public class UserController {
         return Mono.just("Welcome to reactive world ~");     // 【改】使用Mono.just生成响应式数据
     }
 
-
     /**
-     * @api {POST} /hello 添加用户需求
+     * @api {POST} /add 添加用户
      * @apiVersion 1.0.0
      * @apiGroup NEED
-     *
-     * @apiParam {String} need_name 需求者名称-非空
-     * @apiParam {String} e_mail 用户邮箱-非空邮箱格式
-     * @apiParam  {String} phone 用户电话-非空
-     * @apiParam {String} company_name 需求公司名称-非空
-     * @apiParam  {String} needs_desc 需求描述-非空
-     *
-     * @apiSuccess {Object} code 返回码
-     * @apiSuccess {Object} reason  中文解释
-     * @apiSuccess {String[]} data  返回数据
-     *
+     * @apiParam {String} name 名称-非空
+     * @apiParam {String} phone 用户电话-非空
      * @apiSuccessExample {json} Success-Response:
-     *     HTTP/1.1 200 OK
-     *     {
-     *          "code":0,
-     *          "reason":"需求已经提交了，我们的工作人员会在2个工作日内和您取得联系!",
-     *          "data":[]
-     *      }
+     * HTTP/1.1 200 OK
+     * {
+     * "code":0,
+     * "data":[]
+     * }
      */
 
     @PostMapping("/add")
-    public Mono<String> add(@RequestBody @Valid User user ) {
+    public Mono<String> add(@RequestBody @Valid User user) {
         System.out.println(user);
         return Mono.just("User added");
     }
